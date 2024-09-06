0fa8a37590f8d3e0aecd330601ab917306de2428
lEl_NiAnsAn <123313005+lEe-NiAnsAn@users.noreply.github.com>
2024-09-06 20:16:42 +0800

    同时调整亮度与对比度

--- Cpp_openCV_tutorial/Demo5.cpp
+++ Cpp_openCV_tutorial/Demo5.cpp
@@ -1,31 +1,49 @@
 ﻿#include "06_tracking_bar.h"
 
+int a;
+int b;
+static int* ptr_l = &a;
+static int* ptr_c = &b;
 static void on_track_l(int light, void* userdata)
 {
-    Mat dst, mo, image;
+    Mat dst, mo, image, temp;
     image = *((Mat*)userdata);
     dst = Mat::zeros(image.size(), image.type());
     mo = Mat::zeros(image.size(), image.type());
+    temp = Mat::zeros(image.size(), image.type());
+    //同步对比度
+    *ptr_l = light;
+    if (*ptr_c >= 50)
+    {
+        double p_c = (*ptr_c - 50) / 50.0 + 1.0;
+        addWeighted(image, p_c, mo, 0, 0, dst);
+    }
+    else
+    {
+        double n_c = 1.0 - (50 - *ptr_c) / 50.0;
+        addWeighted(image, n_c, mo, 0.0, 0, dst);
+    }
 
     if (light > 50) // 变亮
     {
         mo = Scalar((light - 50) * 2, (light - 50) * 2, (light - 50) * 2);
-        add(image, mo, dst);
+        add(dst, mo, temp);
     }
     else // 变暗
     {
         mo = Scalar((50 - light) * 2, (50 - light) * 2, (50 - light) * 2);
-        subtract(image, mo, dst);
+        subtract(dst, mo, temp);
     }
-    imshow("调整", dst);
+    imshow("调整", temp);
 }
 
 static void on_track_c(int contrast, void* userdata)
 {
-    Mat dst, mo, image;
+    Mat dst, mo, image, temp;
     image = *((Mat*)userdata);
     dst = Mat::zeros(image.size(), image.type());
     mo = Mat::zeros(image.size(), image.type());
+    temp = Mat::zeros(image.size(), image.type());
 
     if (contrast >= 50) // 提高对比度
     {
@@ -37,7 +55,21 @@ static void on_track_c(int contrast, void* userdata)
         double n_c = 1.0 - (50 - contrast) / 50.0;
         addWeighted(image, n_c, mo, 0.0, 0, dst);
     }
-    imshow("调整", dst);
+
+    //同步亮度
+    *ptr_c = contrast;
+    if (*ptr_l > 50)
+    {
+        mo = Scalar((*ptr_l - 50) * 2, (*ptr_l - 50) * 2, (*ptr_l - 50) * 2);
+        add(dst, mo, temp);
+    }
+    else
+    {
+        mo = Scalar((50 - *ptr_l) * 2, (50 - *ptr_l) * 2, (50 - *ptr_l) * 2);
+        subtract(dst, mo, temp);
+    }
+
+    imshow("调整", temp);
 }
 
 void Demo5::tracking_bar_demo(Mat& image)
@@ -52,6 +84,7 @@ void Demo5::tracking_bar_demo(Mat& image)
     // 参数五：操作函数——>固定参数
 
     // 调整对比度
+    int now_c = 50; // 设置原对比度值
     on_track_c(now_l, &image);
-    createTrackbar("对比度", "调整", &now_l, max_l, on_track_c, &image);
+    createTrackbar("对比度", "调整", &now_c, max_l, on_track_c, &image);
 }
