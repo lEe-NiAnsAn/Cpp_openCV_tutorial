db64b044ddb9c61dc4cb2d50ce113ce6e72374ce
lEe_NiAnsAn <123313005+lEe-NiAnsAn@users.noreply.github.com>
2024-09-15 18:52:19 +0800

    Add files via upload
    图像归一化

---
+++ Cpp_openCV_tutorial/15_conver&norm.cpp
@@ -0,0 +1,21 @@
+﻿#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+#include "15_conver&norm.h"
+
+int main(int argc, char** argv)
+{
+	Mat src = imread("OIP-C.jpg");
+	if (src.empty())
+	{
+		std::cout << "无法读取到图像……";
+		return -1;
+	}
+	imshow("原图像", src);
+	Demo14 d14;
+	d14.norm_demo(src);
+	waitKey(0);
+	destroyAllWindows();
+
+	return 0;
+}
---
+++ Cpp_openCV_tutorial/15_conver&norm.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+class Demo14
+{
+public:
+	void norm_demo(Mat& image);
+};
