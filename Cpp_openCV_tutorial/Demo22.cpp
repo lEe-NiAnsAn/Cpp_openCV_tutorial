16ddffb35063dea738fa12232d70d0c564192a2e
lEl_NiAnsAn <123313005+lEe-NiAnsAn@users.noreply.github.com>
2024-09-20 15:23:23 +0800

    直方图均衡化

--- Cpp_openCV_tutorial/Demo22.cpp
+++ Cpp_openCV_tutorial/Demo22.cpp
@@ -2,5 +2,13 @@
 
 void Demo22:: hist_eq_demo(Mat& image)
 {
-
+	Mat gray;
+	//直方图均衡化仅支持单通道图像
+	cvtColor(image, gray, COLOR_BGR2GRAY);
+	imshow("原灰度图像", gray);
+	Mat dst;
+	equalizeHist(gray, dst);
+	//直方图均衡化使得图像对比度加强，在一定程度上可增强图像效果
+	//原理：通过算法将原图统计过后直方图的灰度区间进行重新分配，使得各灰度区间均衡化
+	imshow("直方图均衡化", dst);
 }
